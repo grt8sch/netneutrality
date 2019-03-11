7754d8b924002c7f268e823700d4adb2bfbb6c11
grt8sch <31548609+grt8sch@users.noreply.github.com>
2019-03-11 09:19:36 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ $(function(){
     $("nav").toggleClass("show");
     toggleNav = !toggleNav;
   });
+  $("nav > a").each(function(){
+    $(this).click(function(e){
+      e.preventDefault();
+      console.log($($(this).attr("href")).scrollTop());
+      $("html, body").animate({scrollTop:$($(this).attr("href")).offset().top-$("header").height()}, 250);
+    });
+  });
   $("article,section, nav").click(function(e){
     e.stopPropagation();
     if(toggleNav){
